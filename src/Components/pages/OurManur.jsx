296c90b35299d93ed69b835e36af1253afb5fdf7
mahedi6666655555 <mahedinir34678@gmail.com>
2023-05-23 21:56:48 +0600

    paralax doen

--- src/Components/pages/OurManur.jsx
+++ src/Components/pages/OurManur.jsx
@@ -3,6 +3,8 @@
 
 
 import { Helmet } from 'react-helmet-async';
+import Cover from './compos/Cover';
+import SingleDatas from './compos/SingleDatas';
 
 const OurManur = () => {
 
@@ -11,9 +13,21 @@ const OurManur = () => {
 
             <Helmet>
                 <title>Bistro Boss | Manue</title>
-                
+
             </Helmet>
-            
+            {/* helmet ends */}
+
+
+            <div>
+
+                <Cover></Cover>
+                
+                <Cover></Cover>
+
+                <Cover></Cover>
+
+            </div>
+
 
         </div>
     );
